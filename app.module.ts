822ef2197c5fbd996224dd5025c4e2ea2604103a
DA M1 52 <dam152@DA-learners-MacBook-Air.local>
2022-06-03 09:53:12 +0200

    initial commit

---
+++ app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ServicesComponent } from './Components/services/services.component';
+import { ContactsComponent } from './Components/contacts/contacts.component';
+import { TeamComponent } from './Components/team/team.component';
+import { PortfolioComponent } from './Components/portfolio/portfolio.component';
+import { NavComponent } from './Components/nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactsComponent,
+    TeamComponent,
+    PortfolioComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
